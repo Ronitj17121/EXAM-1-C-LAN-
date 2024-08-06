2a7a1c30fd458b5af7ca72fe94502edc3fb4caea
RONIT JARIWALA <ronitjariwala2006@gmail.com>
2024-08-06 12:18:21 +0530

    Add files via upload

---
+++ exm1q5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main()
+
+{
+	int perameter,rectangle,height,widhth;
+	
+	printf("Enter a rectengle height : ");
+	scanf("%f",&height);
+	printf("Enter a rectegle widhth : ");
+	scanf("%f",&widhth);
+	
+	
+	printf("%d = %d * %d",&perameter,&height,&widhth);
+	scanf("%d=%d*%d",&perameter,height,widhth);
+}
